3fef586773933be2b64ed7d025bde25c816a16f9
Lee0701 <hgl1002@naver.com>
2021-10-28 09:04:56 +0900

    Add hardware key event original event field

--- app/src/main/java/io/github/lee0701/inputmethod/fusion/event/HardwareKeyEvent.kt
+++ app/src/main/java/io/github/lee0701/inputmethod/fusion/event/HardwareKeyEvent.kt
@@ -1,14 +1,24 @@
 package io.github.lee0701.inputmethod.fusion.event
 
 data class HardwareKeyEvent(
-    val type: Type,
-    override val keyCode: Int?,
-    override val char: Char?,
+    val originalEvent: android.view.KeyEvent,
+    val type: Type = Type.valueOf(originalEvent.action),
+    override val keyCode: Int? = originalEvent.keyCode,
+    override val char: Char? = originalEvent.unicodeChar.toChar(),
     override val text: String?,
-    val modifierState: ModifierState,
+    val modifierState: ModifierState = ModifierState(originalEvent),
     val repeatCount: Int = 0,
 ): KeyEvent {
     enum class Type {
-        PRESS, RELEASE, REPEAT
+        NONE, PRESS, RELEASE, REPEAT;
+        companion object {
+            fun valueOf(value: Int): Type {
+                return when(value) {
+                    android.view.KeyEvent.ACTION_DOWN -> PRESS
+                    android.view.KeyEvent.ACTION_UP -> RELEASE
+                    else -> NONE
+                }
+            }
+        }
     }
 }
--- app/src/main/java/io/github/lee0701/inputmethod/fusion/event/ModifierState.kt
+++ app/src/main/java/io/github/lee0701/inputmethod/fusion/event/ModifierState.kt
@@ -1,9 +1,13 @@
 package io.github.lee0701.inputmethod.fusion.event
 
+import android.view.KeyEvent
+
 data class ModifierState(
     val shift: Boolean = false,
     val alt: Boolean = false,
     val caps: Boolean = false,
     val control: Boolean = false,
     val meta: Boolean = false,
-)
+) {
+    constructor(keyEvent: KeyEvent): this(keyEvent.isShiftPressed, keyEvent.isAltPressed, keyEvent.isCapsLockOn)
+}
